26e6c2aad005979dd7984766012c90fa7a6925f0
iesusdavila <iesusdavila@gmail.com>
2023-08-05 16:17:43 -0500

    Implementacion del algoritmo SIFT y Lucas-Kanade

--- Proyectos/Deteccion de objetos/detectar_objetos.py
+++ Proyectos/Deteccion de objetos/detectar_objetos.py
@@ -1,26 +1,46 @@
 import cv2 as cv
+import numpy as np
 
 video = cv.VideoCapture(0)
 
-haar_cascade = cv.CascadeClassifier('../../Proyecto - Reconocimiento Facial/haar_face.xml')
+sift = cv.SIFT_create()
+
+# Variables para el seguimiento
+old_gray = None
+old_keypoints = None
+mask = None
 
 while video.isOpened():
-    isTrue, frame = video.read()    
+    isTrue, frame = video.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    
-    i=0
-    
-    faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
-            
-    for (x,y,w,h) in faces_rect:
-        i+=1
-        cv.putText(frame, "Persona "+str(i), (x,y), cv.FONT_HERSHEY_COMPLEX, 1.1, (0,255,0), thickness=2)
-        cv.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),1)
-    
-    if isTrue:
+
+    if old_gray is None:
+        # Si es el primer fotograma, inicializamos las variables para el seguimiento
+        old_gray = gray
+        keypoints, descriptors = sift.detectAndCompute(gray, None)
+        old_keypoints = np.array([k.pt for k in keypoints], dtype=np.float32).reshape(-1, 1, 2)
+        mask = np.zeros_like(frame)
+    else:
+        # Si ya se han detectado keypoints en el primer fotograma, continuamos con el seguimiento
+        keypoints, st, err = cv.calcOpticalFlowPyrLK(old_gray, gray, old_keypoints, None)
+        good_new = keypoints[st == 1]
+        good_old = old_keypoints[st == 1]
+
+        # Dibujar las líneas de seguimiento en el fotograma actual
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            #mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), (0, 255, 0), 2)
+            frame = cv.circle(frame, (int(a), int(b)), 5, (0, 0, 255), -1)
+
+        img = cv.add(frame, mask)
         cv.imshow('Video', frame)
 
-    if cv.waitKey(20) & 0xFF==ord('q'):
+        # Actualizar el fotograma anterior y keypoints para el siguiente ciclo
+        old_gray = gray.copy()
+        old_keypoints = good_new.reshape(-1, 1, 2)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 video.release()
