3068fb22ea5b21f5c928da62022082ff0fc9860d
iesusdavila <iesusdavila@gmail.com>
2023-08-07 16:37:03 -0500

    Algoritmo para subir/bajar volumen sin tacto

---
+++ Proyectos/Control Volumen/HandTrackingModule.py
@@ -0,0 +1,129 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+
+class handDetector():
+    
+    def __init__(self, mode=False, maxManos=2, minDetectConf=0.5, minTrackingConf=0.5):
+        self.mode = mode
+        self.maxManos = maxManos
+        self.minDetectConf = minDetectConf
+        self.minTrackingConf = minTrackingConf
+        
+        self.mpManos = mp.solutions.hands
+        self.manos = self.mpManos.Hands(self.mode,self.maxManos,
+                                        1,self.minDetectConf,self.minTrackingConf)
+        self.mpDibujo = mp.solutions.drawing_utils
+        
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def encontrarManos(self, frame, dibujarTrazos=True):
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        self.resultados = self.manos.process(frameRGB)
+        
+        if self.resultados.multi_hand_landmarks:
+            for manosPtsRef in self.resultados.multi_hand_landmarks:
+                if dibujarTrazos:
+                    self.mpDibujo.draw_landmarks(frame, manosPtsRef, self.mpManos.HAND_CONNECTIONS)
+        return frame
+    
+    def encontrarPosicion(self, frame, numMano=0, dibujarTrazos=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.ptRefList = []
+        
+        manoSeleccionada = ""
+        
+        if self.resultados.multi_hand_landmarks:
+            manoSeleccionada = self.resultados.multi_hand_landmarks[numMano]
+        
+        if manoSeleccionada != "":
+            for id, ptRef in enumerate(manoSeleccionada.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(ptRef.x * w), int(ptRef.y * h)
+                
+                xList.append(cx)
+                yList.append(cy)
+                self.ptRefList.append([id, cx, cy])
+                
+                if dibujarTrazos:
+                    cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            
+            bbox = xmin, ymin, xmax, ymax
+        
+            if dibujarTrazos:
+                cv2.rectangle(frame, (bbox[0] - 20, bbox[1] - 20),
+                              (bbox[2] + 20, bbox[3] + 20), (0, 0, 255), 2)
+        
+        return self.ptRefList, bbox
+    
+    def dedosArriba(self):
+        fingers = []
+        
+        print(self.ptRefList)
+        
+        if len(self.ptRefList) != 0:
+            if self.ptRefList[self.tipIds[0]][1] > self.ptRefList[self.tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+            # 4 Fingers
+            for id in range(1, 5):
+                if self.ptRefList[self.tipIds[id]][2] < self.ptRefList[self.tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            return fingers
+        
+    def calcularDistancia(self, pt1, pt2, frame, dibujarTrazos=True):
+        x1, y1 = self.ptRefList[pt1][1], self.ptRefList[pt1][2]
+        x2, y2 = self.ptRefList[pt2][1], self.ptRefList[pt2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        
+        if dibujarTrazos:
+            cv2.circle(frame, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(frame, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(frame, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, frame, [x1, y1, x2, y2, cx, cy]
+    
+def main():
+    video = cv2.VideoCapture(0)
+    
+    tiempo_pasado = 0
+    tiempo_actual = 0
+    
+    detectorManos = handDetector()
+    
+    while True:
+        isSuccess, frame = video.read()
+        
+        tiempo_actual = time.time()
+        fps = 1/(tiempo_actual - tiempo_pasado)
+        tiempo_pasado = tiempo_actual
+        
+        frame = detectorManos.encontrarManos(frame)
+        
+        ptRefList = detectorManos.encontrarPosicion(frame)
+        
+        detectorManos.dedosArriba()
+                
+        cv2.putText(frame, str(int(fps)), (10,30), cv2.FONT_HERSHEY_COMPLEX, 1.1, (0,0,0))
+        cv2.imshow("Frame del video", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break 
+    
+    # Libera los recursos y cierra las ventanas
+    video.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
--- Proyectos/Control Volumen/control_volumen_manos.py
+++ Proyectos/Control Volumen/control_volumen_manos.py
@@ -2,21 +2,18 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
-import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-################################
-wCam, hCam = 640, 480
-################################
+wCam, hCam = 1024, 720
 
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
-detector = htm.handDetector(detectionCon=0.7)
+detector = htm.handDetector(minDetectConf=0.7, maxManos=1)
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
@@ -30,43 +27,56 @@ maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
+area = 0
+colorVol = (255, 0, 0)
+
 while True:
     success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
 
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+    # Find Hand
+    img = detector.encontrarManos(img)
+    ptRefList, bbox = detector.encontrarPosicion(img, dibujarTrazos=True)
+    if len(ptRefList) != 0:
 
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        # Filter based on size
+        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
+        # print(area)
+        if 250 < area < 1000:
 
-        length = math.hypot(x2 - x1, y2 - y1)
-        # print(length)
+            # Find Distance between index and Thumb
+            length, img, lineInfo = detector.calcularDistancia(4, 8, img)
+            # print(length)
 
-        # Hand range 50 - 300
-        # Volume Range -65 - 0
+            # Convert Volume
+            volBar = np.interp(length, [50, 200], [400, 150])
+            volPer = np.interp(length, [50, 200], [0, 100])
 
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volBar = np.interp(length, [50, 300], [400, 150])
-        volPer = np.interp(length, [50, 300], [0, 100])
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
+            # Reduce Resolution to make it smoother
+            smoothness = 10
+            volPer = smoothness * round(volPer / smoothness)
 
-        if length < 50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+            # Check fingers up
+            fingers = detector.dedosArriba()
+            # print(fingers)
 
+            # If pinky is down set volume
+            if not fingers[4]:
+                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                colorVol = (0, 255, 0)
+            else:
+                colorVol = (255, 0, 0)
+
+    # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
     cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, colorVol, 3)
 
-
+    # Frame rate
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
@@ -74,4 +84,9 @@ while True:
                 1, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+
+# Libera los recursos y cierra las ventanas
+cap.release()
+cv2.destroyAllWindows()
