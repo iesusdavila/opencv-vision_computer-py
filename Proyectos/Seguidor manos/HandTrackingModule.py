46e758a0d3c8e44d878d4e0b9eced6e2d2f0c4c8
iesusdavila <iesusdavila@gmail.com>
2023-08-07 12:50:46 -0500

    Metodo para conocer las posiciones de los puntos de referencia de las manos

--- Proyectos/Seguidor manos/HandTrackingModule.py
+++ Proyectos/Seguidor manos/HandTrackingModule.py
@@ -23,15 +23,43 @@ class handDetector():
         
         if self.resultados.multi_hand_landmarks:
             for manosPtsRef in self.resultados.multi_hand_landmarks:
-                # for id, ptRef in enumerate(manosPtsRef.landmark):
-                #     h, w, c = frame.shape
-                #     cx, cy = int(ptRef.x*w), int(ptRef.y*h)
-                #     print(id,cx,cy)
-                    
-                #     cv2.circle(frame, (cx,cy), 15, (255,0,255), cv2.FILLED)
                 if dibujarTrazos:
                     self.mpDibujo.draw_landmarks(frame, manosPtsRef, self.mpManos.HAND_CONNECTIONS)
         return frame
+    
+    def encontrarPosicion(self, frame, numMano=0, dibujarTrazos=True):
+        xList = []
+        yList = []
+        bbox = []
+        self.ptRefList = []
+        
+        manoSeleccionada = ""
+        
+        if self.resultados.multi_hand_landmarks:
+            manoSeleccionada = self.resultados.multi_hand_landmarks[numMano]
+        
+        if manoSeleccionada != "":
+            for id, ptRef in enumerate(manoSeleccionada.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(ptRef.x * w), int(ptRef.y * h)
+                
+                xList.append(cx)
+                yList.append(cy)
+                self.ptRefList.append([id, cx, cy])
+                
+                if dibujarTrazos:
+                    cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            
+            bbox = xmin, ymin, xmax, ymax
+        
+            if dibujarTrazos:
+                cv2.rectangle(frame, (bbox[0] - 20, bbox[1] - 20),
+                              (bbox[2] + 20, bbox[3] + 20), (0, 0, 255), 2)
+        
+        return self.ptRefList, bbox
 
     
 def main():
@@ -50,7 +78,11 @@ def main():
         tiempo_pasado = tiempo_actual
         
         frame = detectorManos.encontrarManos(frame)
-                
+        
+        ptRefList = detectorManos.encontrarPosicion(frame)
+        
+        #print(ptRefList)
+        
         cv2.putText(frame, str(int(fps)), (10,30), cv2.FONT_HERSHEY_COMPLEX, 1.1, (0,0,0))
         cv2.imshow("Frame del video", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
