b9c117c1519cdd82755cc6f69dd30910a717c73d
iesusdavila <iesusdavila@gmail.com>
2023-08-09 18:13:43 -0500

    Mejora en el diseño y comentarios

--- Proyectos/Control Volumen/control_volumen_manos.py
+++ Proyectos/Control Volumen/control_volumen_manos.py
@@ -17,8 +17,7 @@ time_pasado = 0
 detector = htm.handDetector(minDetectConf=0.7, maxManos=1)
 
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 volRange = volume.GetVolumeRange()
 
@@ -36,16 +35,15 @@ colorVol = (255, 0, 0)
 while True:
     isSuccess, frame = cap.read()
 
-    # Find Hand
+    # Encontrar mano
     frame = detector.encontrarManos(frame)
     ptRefList, bbox = detector.encontrarPosicion(frame, dibujarTrazos=True)
     if len(ptRefList) != 0:
 
         # Area del cuadrado de la mano
         area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
-        print(area)
         
-        # Programa funcionara solo entre 150 y 1000
+        # Programa funcionara solo entre un area de 150 y 1000
         if 150 < area < 1000:
             
             # 4 indica la punta del pulgar, 8 indica la punta del indice
@@ -55,29 +53,32 @@ while True:
             volBar = np.interp(longitud, [50, 200], [400, 150])
             volPer = np.interp(longitud, [50, 200], [0, 100])
 
-            # Reduce Resolution to make it smoother
-            smoothness = 10
+            # El volumen aumentara de 2 en 2
+            smoothness = 2
             volPer = smoothness * round(volPer / smoothness)
 
-            # Check fingers up
+            # Verificar si tenemos los dedos arriba
             fingers = detector.dedosArriba()
 
-            # If pinky is down set volume
+            # Validar si el dedo pulgar esta arriba
             if not fingers[4]:
                 volume.SetMasterVolumeLevelScalar(volPer / 100, None)
-                cv2.circle(frame, (lineInfo[4], lineInfo[5]), 15, (155, 255, 50), cv2.FILLED)
+                cv2.circle(frame, (lineInfo[4], lineInfo[5]), 5, (155, 255, 50), cv2.FILLED)
                 colorVol = (0, 255, 0)
             else:
                 colorVol = (255, 0, 0)
 
-    # Dibujos
+    # ------- Dibujos -------
+    # Dibujo del cuadro de volumen
     cv2.rectangle(frame, (50, 150), (85, 400), (53, 53, 47), 2)
     cv2.rectangle(frame, (50, int(volBar)), (85, 400), (76, 76, 65), cv2.FILLED)
+    # Numero del nivel del volumen
     cv2.putText(frame, f'{int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (53, 53, 47), 2)
+    # Volumen seteado
     cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
-    cv2.putText(frame, f'Vol Set: {int(cVol)}', (600, 30), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 2)
+    cv2.putText(frame, f'Vol Set: {int(cVol)}', (570, 30), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 2)
 
-    # Frame rate
+    # Calculo de FPS
     time_actual = time.time()
     fps = 1 / (time_actual - time_pasado)
     time_pasado = time_actual
