2b824cf9de7886e01e6ad17380aae8b38733601d
iesusdavila <iesusdavila@gmail.com>
2023-09-10 14:06:26 -0500

    Mejora la logica de deteccion de imagenes en video - Proyecto pasado

--- Proyectos/Sustraer imagenes de video/imagenes_video.py
+++ Proyectos/Sustraer imagenes de video/imagenes_video.py
@@ -16,13 +16,11 @@ while True:
     # numero minimo de vecinos requeridos para que se detecte una region como una cara
     faces = faceClassif.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
     
-    i = 0
-    for (x,y,w,h) in faces:
-        i=i+1
-        cv2.putText(frame, "Persona "+str(i), (x,y), cv2.FONT_HERSHEY_COMPLEX, 1.2, (0,255,0), thickness=3)
+    for (i,(x,y,w,h)) in enumerate(faces):
+        cv2.putText(frame, "Persona "+str(i+1), (x,y), cv2.FONT_HERSHEY_COMPLEX, 1.2, (0,255,0), thickness=3)
         cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
     
-    cv2.putText(frame, "Personas encontradas "+str(i), (20,20), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=3)
+    cv2.putText(frame, "Personas encontradas "+str(i+1), (20,20), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=3)
     
     cv2.imshow('Video en tiempo real',frame)
 	
