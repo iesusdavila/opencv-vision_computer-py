1a3b167168efd0bd1d8ff70fe5b602fd1b7a98c1
iesusdavila <iesusdavila@gmail.com>
2023-09-13 15:50:07 -0500

    Mejora en un proyecto anterior para contar cartas añadiendo conceptos de una mejor forma

--- Proyectos/Contador cartas/contador_cartas.py
+++ Proyectos/Contador cartas/contador_cartas.py
@@ -1,15 +1,27 @@
 import cv2
 import numpy as np
 
-imagen = cv2.imread('cartas_3.jpg')
-imagen = cv2.resize(imagen, (512, 512), interpolation=cv2.INTER_CUBIC)
+imagen = cv2.imread('cartas_2.jpg')
+imagen = cv2.resize(imagen, (512, 512), interpolation=cv2.INTER_AREA)
 grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-bordes = cv2.Canny(grises, 150, 450, True)
+cv2.imshow('Grises', grises)
+
+# NUEVO - LIMPIEZA DE LA IMAGEN PARA DETECTAR SUS CONTORNOS
+# EXACTITUD DEL 100%, Antes marcaba 850 cartas y ahora marca exactamente 4 cartas
+blur = cv2.GaussianBlur(grises, (5,5), cv2.BORDER_TRANSPARENT)
+cv2.imshow('Blur', blur)
+dilated = cv2.dilate(blur, (8,8), iterations=20)
+cv2.imshow('Dilatacion',dilated)
+# FIN DE NUEVO CODIGO
+
+# bordes = cv2.Canny(dilated, 250, 500, True)
+ret, bordes = cv2.threshold(dilated, 250, 500, 1)
 
 
 # RETR_TREE: modo de recuperación de contornos
 # CHAIN_APPROX_SIMPLE: método de aproximación de contornos
-ctns, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+# ctns, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+ctns, _ = cv2.findContours(bordes, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) 
 
 print(ctns[0].shape)
 
