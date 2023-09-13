5c66da3ca242b6be1b1db3fb4ab1bf67d26e30b3
iesusdavila <iesusdavila@gmail.com>
2023-09-13 15:26:10 -0500

    Mini proyecto - Coincidir figuras sobre una imagen

--- Seccion #2 - Avanzados/coincidir_patrones/proy_coincidar_diferentes_formas.py
+++ Seccion #2 - Avanzados/coincidir_patrones/proy_coincidar_diferentes_formas.py
@@ -1,7 +1,16 @@
 import numpy as np
 import cv2 as cv
 
-# Load and then gray scale image
+def calcular_centroide(M):
+    M = cv.moments(cnt)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    
+    return (cx,cy)
+
+def definir_figura(shape_name, image, cnt, color_shape, color_text, cx, cy, desfase_x = 0, desfase_y = 0):    
+    cv.drawContours(image,[cnt],0,color_shape,-1)
+    cv.putText(image, shape_name, (cx-desfase_x, cy-desfase_y), cv.FONT_HERSHEY_SIMPLEX, 1, color_text, 1)
 
 image = cv.imread('../../Resources/Images/someshapes.jpg')
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -11,10 +20,8 @@ cv.waitKey(0)
 
 ret, thresh = cv.threshold(gray, 127, 255, 1)
 
-# Encontrar los contornos de la imagen de referencia
 contours, hierarchy = cv.findContours(thresh.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
-# Recorrer todos los contornos hallados
 for cnt in contours:
     
     # Obtener la aproximacion por poligono
@@ -28,57 +35,30 @@ for cnt in contours:
     # closed: indica si la figura es cerrada o abierta
     approx = cv.approxPolyDP(cnt, accuracy, True)
     
+    # Encontrar el centroide de la imagen
+    M = cv.moments(cnt)
+    cx, cy = calcular_centroide(M)
+    
+    # VALIDACIONES PARA CONOCER QUE TIPO DE FIGURAS TENEMOS
+    # Dependiendo del numero de vertices sabemos que figura se tienes
     if len(approx) == 3:
-        shape_name = "Triangulo"
-        cv.drawContours(image,[cnt],0,(0,255,0),-1)
-        
-        # Find contour center to place text at the center
-        M = cv.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        cv.putText(image, shape_name, (cx-50, cy), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+        definir_figura(shape_name = "Triangulo", image=image, cnt=cnt, color_shape=(0,255,0), color_text=(0,0,0), cx=cx, cy=cy, desfase_x=70)
     
     elif len(approx) == 4:
         x,y,w,h = cv.boundingRect(cnt)
-        M = cv.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
         
-        # Check to see if 4-side polygon is square or rectangle
-        # cv.boundingRect returns the top left and then width and 
         if abs(w-h) <= 3:
-            shape_name = "Cuadrado"
-            
-            # Find contour center to place text at the center
-            cv.drawContours(image, [cnt], 0, (0, 125 ,255), -1)
-            cv.putText(image, shape_name, (cx-50, cy), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+            definir_figura(shape_name = "Cuadrado", image=image, cnt=cnt, color_shape=(0, 125 ,255), color_text=(0,0,0), cx=cx, cy=cy, desfase_x=70)
         else:
-            shape_name = "Rectangulo"
-            
-            # Find contour center to place text at the center
-            cv.drawContours(image, [cnt], 0, (0, 0, 255), -1)
-            M = cv.moments(cnt)
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv.putText(image, shape_name, (cx-50, cy), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+            definir_figura(shape_name = "Rectangulo", image=image, cnt=cnt, color_shape=(0, 0, 255), color_text=(0,0,0), cx=cx, cy=cy, desfase_x=70)
+
             
     elif len(approx) == 10:
-        shape_name = "Estrella"
-        cv.drawContours(image, [cnt], 0, (255, 255, 0), -1)
-        M = cv.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        cv.putText(image, shape_name, (cx-50, cy), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-        
-        
+        definir_figura(shape_name = "Estrella", image=image, cnt=cnt, color_shape=(255, 255, 0), color_text=(0,0,0), cx=cx, cy=cy, desfase_x=70)
         
     elif len(approx) >= 15:
-        shape_name = "Circulo"
-        cv.drawContours(image, [cnt], 0, (0, 255, 255), -1)
-        M = cv.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        cv.putText(image, shape_name, (cx-50, cy), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
+        definir_figura(shape_name = "Circulo", image=image, cnt=cnt, color_shape=(0, 255, 255), color_text=(0,0,0), cx=cx, cy=cy, desfase_x=50)
+
 
     cv.imshow('Identicacion de formas',image)
     cv.waitKey(0)
